20bb0daa7ab30f127efdb30dc2f185e5ab112ab8
chuandangit <50528224+chuandangit@users.noreply.github.com>
2019-09-20 17:19:19 +0700

    aa

--- VirtualOutfitRoomWebAdmin/src/app/service/displayservice.service.spec.ts
+++ VirtualOutfitRoomWebAdmin/src/app/service/displayservice.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DisplayserviceService } from './displayservice.service';
+import { DisplayService } from './displayservice.service';
 
 describe('DisplayserviceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: DisplayserviceService = TestBed.get(DisplayserviceService);
+    const service: DisplayService = TestBed.get(DisplayService);
     expect(service).toBeTruthy();
   });
 });
