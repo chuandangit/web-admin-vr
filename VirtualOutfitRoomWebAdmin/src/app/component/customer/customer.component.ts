d7f49abb8c333741044ded7028a09bb04e3413bd
chuandangit <50528224+chuandangit@users.noreply.github.com>
2019-09-24 00:05:46 +0700

    bbbb

--- VirtualOutfitRoomWebAdmin/src/app/component/customer/customer.component.ts
+++ VirtualOutfitRoomWebAdmin/src/app/component/customer/customer.component.ts
@@ -1,36 +1,15 @@
-<<<<<<< HEAD
-
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatPaginator, MatSort } from '@angular/material';
-import {DataTableDataSource } from '../other/data-table/data-table-datasource'
-import { from } from 'rxjs';
-=======
 import { Component, OnInit } from '@angular/core';
->>>>>>> parent of c71294d... Merge branch 'master' of https://github.com/chitrung252/VirtualOutfitRoomWebAdmin
 
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
   styleUrls: ['./customer.component.css']
 })
-<<<<<<< HEAD
-export class CustomerComponent implements AfterViewInit {
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-  dataSource: DataTableDataSource;
-
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
-
-  ngAfterViewInit() {
-    this.dataSource = new DataTableDataSource(this.paginator, this.sort);
-=======
 export class CustomerComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
->>>>>>> parent of c71294d... Merge branch 'master' of https://github.com/chitrung252/VirtualOutfitRoomWebAdmin
   }
 
 }
