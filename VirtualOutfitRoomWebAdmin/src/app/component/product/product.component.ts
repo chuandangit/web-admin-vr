16e6e9698006712b886c1972e36a738ac80cb669
chuandangit <50528224+chuandangit@users.noreply.github.com>
2019-09-24 13:03:21 +0700

    aaa

--- VirtualOutfitRoomWebAdmin/src/app/component/product/product.component.ts
+++ VirtualOutfitRoomWebAdmin/src/app/component/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../model/product';
 
 @Component({
   selector: 'app-product',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-
+  PRODUCT : Product[] = [{id : 100,  name : "T-shirt red" , price : 10000 , quantity : 10 , status : "disabled"},
+  {id : 101,  name : "T-shirt yellow" , price : 20000 , quantity : 10 , status : "enabled"}
+                              ]
 
   constructor() { }
 
