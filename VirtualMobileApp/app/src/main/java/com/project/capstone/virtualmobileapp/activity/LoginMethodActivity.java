279705e03be0b41d8e1db417934b8a6c8fc59adc
Giang Tran Hoang <giangthse62424@fpt.edu.vn>
2019-10-14 15:53:18 +0700

    update authentication

---
+++ VirtualMobileApp/app/src/main/java/com/project/capstone/virtualmobileapp/activity/LoginMethodActivity.java
@@ -0,0 +1,15 @@
+package com.project.capstone.virtualmobileapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.capstone.virtualmobileapp.R;
+
+public class LoginMethodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_method);
+    }
+}
