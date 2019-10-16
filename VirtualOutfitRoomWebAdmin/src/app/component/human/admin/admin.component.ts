aab92bd47b90bc0024ac41c260d2e83281cee112
chuandangit <50528224+chuandangit@users.noreply.github.com>
2019-10-16 18:40:15 +0700

    aa

--- VirtualOutfitRoomWebAdmin/src/app/component/human/admin/admin.component.ts
+++ VirtualOutfitRoomWebAdmin/src/app/component/human/admin/admin.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ModalService } from './../../../service/modal.service';
+import { Component, OnInit , Input } from '@angular/core';
 import {Account} from '../../../model/account';
+import { AdminDetailComponent } from '../admin-detail/admin-detail.component';
+
 
 @Component({
   selector: 'app-admin',
@@ -8,17 +11,20 @@ import {Account} from '../../../model/account';
 })
 export class AdminComponent implements OnInit {
 
-  
+  @Input() inputs;
+  admin : Account;
+  pos : Number;
   ADMIN : Account[] = [{id : 6 ,  firstname : 'admin' , lastname : 'admin' , username : 'admin' , address : '43/5 vuon lai , q12, hcm' ,status: "active" , datecreated : "10-10-2019"}
                             ]
 
-  constructor() { }
+  constructor(private modalService : ModalService) { }
 
   ngOnInit() {
   }
 
-  openAdDetail(){
-
+  openAdDetail(id){
+      
+      this.modalService.init(AdminDetailComponent, this.inputs , [])
   }
 
 }
